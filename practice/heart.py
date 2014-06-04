a06ec6f92d478c1ec2da344d7326e66bb73051c7
AlicJ <zichen.jiang@yahoo.com>
2014-06-04 09:29:36 -0400

    changes on user choice

--- practice/heart.py
+++ practice/heart.py
@@ -9,9 +9,10 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 # add lines into a list and then draws them to see if it helps with the speed
-from math import *
+from math import cos, sin, pi
 from graphics import *
-from random import *
+from random import randint
+from time import sleep
 
 def heartEq(t):
     x = 16*sin(t)**3
@@ -35,10 +36,8 @@ def getLength(line):
 
 
 def main():
-    win = GraphWin("Heart", 600, 600)
-    w = 20
-    win.setCoords(-w,-w,w,w)
 
+    w = 20
     listOfPoints = []
     numOfPoints = 0
     numOfLines = 0
@@ -49,24 +48,22 @@ def main():
     preference = "" #random or ordered
     choice = " "
     outline = False
+    listOfLinesLeft = []
+    listOfLinesRight = []
 
-    choice = input("Which one do you prefer, (a)randomness or (b)order?")
+    choice = input("Which one do you prefer, (a)order, (b)randomness, or (c)surprise me?\n")
+##    if choice == "a" or choice == "A" or choice == "random" or choice == "1" or choice == "randomness": preference = "random"
+##    else: preference = "order"
 
-    print("choice: ", choice)
-
-    if choice == "a" or choice == "A" or choice == "random" or choice == "1" or choice == "randomness":
-        preference = "random"
-    else:
-        preference = "order"
-
-    print("preference: ", preference)
+    win = GraphWin("Heart", 600, 600)
+    win.setCoords(-w,-w,w,w)
 
     # draws the points on the heart
     for i in drange(-pi, pi, pointStep):
         point = heartEq(i)
-        #print(point.getX(),point.getY())
         listOfPoints.append(point)
-        point.setFill("red")
+        #print(point.getX(),point.getY())
+        #point.setFill("red")
         #point.draw(win)
 
     numOfPoints = len(listOfPoints)
@@ -80,37 +77,52 @@ def main():
             line.draw(win)
 
 
-    if preference =="order":
+    if choice =="a": # order
         # connects the dots on the left half
         for i in range(numOfPoints//2):
             for j in range(1,numOfPoints//2,lineStep):
                 line = Line(listOfPoints[j], listOfPoints[i])
-                totalLength +=  getLength(line)
                 line.setFill("red")
-                line.draw(win)
-                numOfLines += 1
+                listOfLinesLeft.append(line)
+                #totalLength +=  getLength(line)
+                #numOfLines += 1
 
         # connects the dots on the right half
         for i in reversed(range(numOfPoints//2, numOfPoints,1)):
             for j in reversed(range(numOfPoints//2+1, numOfPoints,lineStep)):
                 line = Line(listOfPoints[j], listOfPoints[i])
-                totalLength +=  getLength(line)
                 line.setFill("red")
-                line.draw(win)
-                numOfLines += 1
+                listOfLinesRight.append(line)
+                #numOfLines += 1
+                #totalLength +=  getLength(line)
 
-    elif preference == "random":
+        for i in range(len(listOfLinesLeft)):
+            listOfLinesLeft[i].draw(win)
+            listOfLinesRight[i].draw(win)
+
+    elif choice == "b": # random
         # connects the dots randomly
         for i in range(800):
             pt1 = randint(0, numOfPoints-1)
             pt2 = randint(0, numOfPoints-1)
-            while abs(pt2-pt1) > 6:
+            while abs(pt2-pt1) > 6 or abs(pt2-pt1) < 4:
                 pt2 = randint(0, numOfPoints-1)
             line = Line(listOfPoints[pt1], listOfPoints[pt2])
-            totalLength +=  getLength(line)
+            #totalLength +=  getLength(line)
             line.setFill("red")
             line.draw(win)
-            numOfLines += 1
+            sleep(0.0001)
+            #numOfLines += 1
+
+    elif choice == "c":
+        for i in range(5,numOfPoints,1):
+            for j in range(1,8,2):
+                line = Line(listOfPoints[i], listOfPoints[i-j])
+                line.setFill("red")
+                line.draw(win)
+                sleep(0.01)
+
+
 
     win.getMouse()
     win.close()
